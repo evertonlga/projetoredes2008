c500c49145a4898f74f7c850a9f281a61b586b7a
vinicius.aguiar <vinicius.aguiar@9001ecb2-ed51-0410-9fb0-c11a5528f6ea>
2008-08-08 17:49:47 +0000

    comentarios da classe

--- Redes/src/main/Main.java
+++ Redes/src/main/Main.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 import controle.Controlador;
 
+
+/**
+ * Classe a ser instanciada para executar o simulador
+ * @author Everton Galdino
+ * @author Vinicius Marques
+ *
+ */
 public class Main {
 
-	
+	/**
+	 * para poder instanciar o roteador precisamos passar o identificador para o roteador. As configuracoes serao
+	 * mapeadas a partir dos arquivos de configuracao do simulador.
+	 * o simulador vai carregar apenas as configuracoes referentes ao identificador do roteador  
+	 * Caso nao seja passado nenhum parametro o simulador perguntara qual o roteador sera criado
+	 * @param args identificador do roteador
+	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int id = 0;

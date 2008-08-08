163dbfeaa2e0ce09811bdb4e8decde3ffc1c9ce7
vinicius.aguiar <vinicius.aguiar@9001ecb2-ed51-0410-9fb0-c11a5528f6ea>
2008-08-08 23:35:11 +0000

    comentarios da classe

--- trunk/Redes/src/Util/Util.java
+++ trunk/Redes/src/Util/Util.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import logica.No;
 import logica.RotCelula;
 
+/**
+ * 
+ * @author Everton Galdino
+ * @author Vinicius Marques
+ *
+ */
 public class Util {
 
 	public static final String CONFIG_ROTEADOR = "." + System.getProperty("file.separator")+"configuracao"+
@@ -13,6 +19,11 @@ public class Util {
 					System.getProperty("file.separator")+ "enlaces.config";
 	public static final int RAIO_DA_REDE = 26;
 	
+	/**
+	 * prepara a mensagem para ser enviada 
+	 * @param no destino da mensagem 
+	 * @return mensagem pronta para ser enviada
+	 */
 	public static String codificaDadosDoPacote(No no) {
 		String dados = "";
 		RotCelula [][] tabela = no.getTabela();
@@ -31,6 +42,11 @@ public class Util {
 		return dados.trim()+" .";
 	}
 
+	/**
+	 * extrai o dado do pacote
+	 * @param mensagem mensagem codificada
+	 * @return a mensagem que estava no pacote
+	 */
 	public static Integer[] decodificaDadosPacote(String mensagem) {
 		String[] nums = mensagem.split(" ");
 		Integer[] values = new Integer[limit(nums)];
@@ -48,7 +64,12 @@ public class Util {
 		
 		return values;
 	}
-
+	
+	/**
+	 * conta
+	 * @param nums
+	 * @return
+	 */
 	private static int limit(String[] nums) {
 		for (int i =0; i< nums.length; i++)
 			if (nums[i].startsWith("."))

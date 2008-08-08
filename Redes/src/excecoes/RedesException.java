642c399ccba7a322e558e1e68accdfb5024cadd6
vinicius.aguiar <vinicius.aguiar@9001ecb2-ed51-0410-9fb0-c11a5528f6ea>
2008-08-08 19:14:44 +0000

    comentarios da classe

--- Redes/src/excecoes/RedesException.java
+++ Redes/src/excecoes/RedesException.java
@@ -1,7 +1,16 @@
 package excecoes;
-
+/**
+ * Excecao generica
+ * @author Everton Galdino
+ * @author Vinicius Marques
+ *
+ */
 public class RedesException extends Exception {
 	
+	/**
+	 * Construtor da classe
+	 * @param msg mensagem que sera exibida quando a excecao for lancada
+	 */
 	public RedesException(String msg){
 		super(msg);
 	}

2956d74619276d1333d2b4d35c38ca1409f8408c
vinicius.aguiar <vinicius.aguiar@9001ecb2-ed51-0410-9fb0-c11a5528f6ea>
2008-08-09 00:38:08 +0000

    comentarios da classe

--- trunk/Redes/src/threads/RepeteThread.java
+++ trunk/Redes/src/threads/RepeteThread.java
@@ -2,16 +2,30 @@ package threads;
 
 import logica.BellmanFord;
 
+/**
+ * classe que fica reexecutando uma Thread
+ * @author Everton Galdino
+ * @author Vinicius Marques
+ *
+ */
 public class RepeteThread extends Thread {
 
 	BellmanFord bf;
 	int tempo;
 	
+	/**
+	 * Construtor da classe
+	 * @param bellmanFord objeto que ficara sendo reexecutado
+	 * @param t frequencia de execucao da Thread
+	 */
 	public RepeteThread(BellmanFord bellmanFord, int t) {
 		this.bf = bellmanFord;
 		this.tempo = t;
 	}
 	
+	/**
+	 * inicia a Thread
+	 */
 	public void run(){
     	while (true){
 	    	try{
@@ -28,18 +42,34 @@ public class RepeteThread extends Thread {
     	}
     }
 
+	/**
+	 * retorna a referencia ao objeto que tah executando o Bellman-Ford
+	 * @return a referencia ao objeto que tah executando o Bellman-Ford
+	 */
 	public BellmanFord getBf() {
 		return bf;
 	}
 
+	/**
+	 * altera o objeto que esta sendo reexecutado pela Thread
+	 * @param bf o novo objeto 
+	 */
 	public void setBf(BellmanFord bf) {
 		this.bf = bf;
 	}
-
+	
+	/**
+	 * retorna o tempo de repeticao da thread
+	 * @return a frequencia da thread
+	 */
 	public int getTempo() {
 		return tempo;
 	}
 
+	/**
+	 * altera o tempo de repeticao da thread
+	 * @param tempo a nova frequencia para a Thread
+	 */
 	public void setTempo(int tempo) {
 		this.tempo = tempo;
 	}

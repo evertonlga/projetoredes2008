f00bd4db07609a799cc770081c0f2c8d3988e672
vinicius.aguiar <vinicius.aguiar@9001ecb2-ed51-0410-9fb0-c11a5528f6ea>
2008-08-09 00:40:23 +0000

    comentarios da classe

--- Redes/src/threads/RoteadorThread.java
+++ Redes/src/threads/RoteadorThread.java
@@ -5,16 +5,30 @@ import logica.No;
 import controle.Controlador;
 import excecoes.RedesException;
 
+/**
+ * 
+ * @author Everton Galdino
+ * @author Vinicius Marques
+ *
+ */
 public class RoteadorThread extends Thread {
 
 	private Controlador controlador;
 	private int id;
 	
+	/**
+	 * Construtor da classe
+	 * @param controlador controlador do roteador
+	 * @param id identificador do roteador
+	 */
 	public RoteadorThread(Controlador controlador, int id) {
 		this.controlador = controlador;
 		this.id = id;
 	}
 	
+	/**
+	 * inicializa a thread
+	 */
 	public void run() {
 		controlador.setNoAtual(new No(id));
 		controlador.setBellmanFord(new BellmanFord (controlador.getNoAtual()));
